d2ceef3fdff4a1cbc03529f848a115405abb7bbf
ismael <maelio@live.com>
2024-05-12 20:51:05 -0300

    primeiros testes

---
+++ src/repositories/in-memory-users-repository.ts
@@ -0,0 +1,28 @@
+import { User, Prisma } from '@prisma/client'
+import { UsersRepository } from './users-repository'
+
+export class InMemoryUsersRepository implements UsersRepository {
+  public items: User[] = []
+  async findByEmail(email: string) {
+    const user = this.items.find((item) => item.email === email)
+
+    if (!user) {
+      return null
+    }
+    return user
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = {
+      id: '1-user',
+      name: data.name,
+      email: data.email,
+      created_at: new Date(),
+      password_hash: data.password_hash,
+    }
+
+    this.items.push(user)
+
+    return user
+  }
+}
