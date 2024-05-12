4b69b945c0b81ac9d1f524d211e79cfe13c9a9aa
ismael <maelio@live.com>
2024-05-12 19:09:30 -0300

    tratamento de erros

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
-import { app } from "./app"
+import { app } from './app'
 
-import { env } from "./env"
+import { env } from './env'
 
 app
   .listen({
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     port: env.PORT,
   })
   .then(() => {
-    console.log("🚀 HTTP server running!")
+    console.log('🚀 HTTP server running!')
   })
